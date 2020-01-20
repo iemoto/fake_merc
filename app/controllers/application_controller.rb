2ab40d603ecb431762fd67d0b72b23d97505227f
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-01-20 19:41:59 +0900

    modify rails s時のエラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :Rails.env.production?
+  before_action :basic_auth, if: :production?
+
+def index
+end
 
   private
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == '61_mercari_wc' && password == '61wc'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  root 'tests#index'
 end
