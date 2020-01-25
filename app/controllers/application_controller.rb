70e0dbfe0af72a9989e2a64d07088d90e7597ad0
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-01-25 20:20:24 +0900

    new_user-registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
 
-def index
-end
+  def index
+  end
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+  end
 
   private
   def production?
