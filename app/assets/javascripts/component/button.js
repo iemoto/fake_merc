145d3f55d1dbff6b84c4c8f4771c20b0da58c042
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-02-04 05:47:57 +0900

    Modify JS color change button

--- app/assets/javascripts/component/button.js
+++ app/assets/javascripts/component/button.js
@@ -1,11 +1,21 @@
 $(function(){
   // サイドバーのボタン色変化
-  $(".sideBar__item").on('click', function(e){
-    e.preventDefault();
-    $('.sideBar__item').removeClass('isSideButton');
-    $(this).toggleClass('isSideButton');
-  });
+  
+  // $('.sideBar__item a').each(function(){
+  //   var $href = $(this).attr('href');
+  //   if(location.href.match($href)) {
+  //     $(this).addClass('isSideButton');
+  //   } else {
+  //     $(this).removeClass('isSideButton');
+  //   }
+  // });
 
+  $(".sideBar__item").on('click', function(e){
+    // e.preventDefault();
+    $('.sideBar__item').removeClass('isSideButton');
+    $(this).addClass('isSideButton');
+  });
+  
   // タブ切り替え色変化
   $(".mypageNews__tab").on('click', function(e){
     e.preventDefault();
