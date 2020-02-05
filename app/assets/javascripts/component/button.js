1391a8cc587875d40b5ecb58f8fbcee75dbe44b7
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-02-06 00:14:24 +0900

    Modify jQueryButton

--- app/assets/javascripts/component/button.js
+++ app/assets/javascripts/component/button.js
@@ -1,20 +1,20 @@
 $(function(){
   // サイドバーのボタン色変化
   
-  // $('.sideBar__item a').each(function(){
-  //   var $href = $(this).attr('href');
-  //   if(location.href.match($href)) {
-  //     $(this).addClass('isSideButton');
-  //   } else {
-  //     $(this).removeClass('isSideButton');
-  //   }
-  // });
-
-  $(".sideBar__item").on('click', function(e){
-    // e.preventDefault();
-    $('.sideBar__item').removeClass('isSideButton');
-    $(this).addClass('isSideButton');
+  $('.sideBar__item a').each(function(){
+    var $href = $(this).prop('href');
+    if(location.href.match($href)) {
+      $('.sideBar__item').removeClass('isSideButton');
+      $(this).parent().addClass('isSideButton');
+    } else {
+      $(this).removeClass('isSideButton');
+    }
   });
+
+  // $(".sideBar__item").on('click', function(){
+  //   $('.sideBar__item').removeClass('isSideButton');
+  //   $(this).addClass('isSideButton');
+  // });
   
   // タブ切り替え色変化
   $(".mypageNews__tab").on('click', function(e){
