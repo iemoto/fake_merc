78cd49828f0bc38a2ece1f917b2176b1ad98a71b
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-02-08 18:21:27 +0900

    Addslide item-zone

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.main-contents__notice--slide').slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    prevArrow: '<span class="prev-arrow">&lt;</span>',
+    nextArrow: '<span class="next-arrow">&gt;</span>'
+  });
+});
