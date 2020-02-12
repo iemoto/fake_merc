723ec54f62642e4058e616962e8ea904fdc3b55d
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-02-12 23:57:57 +0900

    会員情報画面

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/:id/show', to: 'mains#show'
 
   # マイページトップページ、本人情報登録画面
-  get '/mypage', to: 'users#index'
+  get '/mypage', to: 'users#new_member'
   get '/mypage/identification', to: 'users#edit'
   patch '/mypage/identification', to: 'users#update'
 
