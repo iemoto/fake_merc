ca895226ab0183592cfd90126e455aca6c335e8c
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-02-15 15:04:58 +0900

    Slide-Refactoring

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( *.svg )
 Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
