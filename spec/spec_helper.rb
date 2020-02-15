0ce3d9df2b4699905d886e7dfbd74da55a7fff09
Furuya-Koichi <seawasekoichi417@icloud.com>
2020-02-15 14:11:32 +0900

    テスト中

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,4 +93,20 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+def facebook_mock(name, email)
+  OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
+    provider: 'facebook',
+    uid: 1234567890,
+    info: {
+      name: name,
+      email: email
+    },
+    credentials: {
+      token: 'hogepiyo1234'
+    }
+  )
+end
+
+
+OmniAuth.config.test_mode = true
 end
