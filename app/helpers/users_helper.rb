1319920a9845a5ce73eea94dc66b8dc98c902902
Furuya-Koichi <seawasekoichi417@icloud.com>
2020-02-16 16:15:35 +0900

    app/helpers/users_helper.rb内の不必要な記述を削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,2 @@
 module UsersHelper
-  def facebook_mock(name, email)
-    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
-      provider: 'facebook',
-      uid: 1234567890,
-      info: {
-        name: name,
-        email: email
-      },
-      credentials: {
-        token: 'hogepiyo1234'
-      }
-    )
-  end
 end
