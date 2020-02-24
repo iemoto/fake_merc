d350cce69f26166b99ed4aed32ba75f3f45aab28
iemoto <57856680+iemoto@users.noreply.github.com>
2020-02-24 19:40:14 +0900

    action hash add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   def index
   end
   
-  def index_login
-  end
-
   def show
     unless user_signed_in? then
       redirect_to root_path
     end
   end
+
+  def edit
+  end
+
+
 end
--- spec/factories/prefecture_addresses.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :prefecture_address do
-    prefecture_id 1
-    name "MyString"
-  end
-end
