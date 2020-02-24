a4bdb6e4f83e917328b54f98f75cddbea1b8a983
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-02-24 13:54:49 +0900

    AddJquery

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,28 @@
+$(function(){
+  //画像用のinputを生成する関数
+  const buildFileField = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="item[images_attributes][${index}][image_url]"
+                    id="item_images_attributes_${index}_image_url">
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
+  //file_fieldのnameに動的なindexをつける為の配列
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+  $('#image-box').on('change', '.js-file', function(e) {
+    //fileIndexの先頭の数字をinputを作る
+    $('#image-box').append(buildFileField(fileIndex[0]));
+    fileIndex.shift();
+    //末尾の数に１足した数を追加する
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  });
+
+  $('#image-box').on('click', '.js-remove', function(){
+    $(this).parent().remove();
+    //画像入力蘭が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  });
+});
