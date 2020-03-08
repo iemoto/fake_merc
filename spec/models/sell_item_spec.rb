d4003d834b9827f4d6050e9a28dc0e15861ce806
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-08 17:50:50 +0900

    Add S3 setting

--- spec/models/sell_item_spec.rb
+++ spec/models/sell_item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SellItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規会員登録' do
+    it 'nick_name,email,passwordが全部ある場合、登録できる' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'nick_nameがない場合、登録できない' do
+      user = build(:user, nick_name: nil)
+      user.valid?
+      expect(user.errors[:nick_name]).to include("can't be blank")
+    end
+
 end
