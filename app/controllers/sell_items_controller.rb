8a7a99d643aa76103f1d0d108f09c4c4c46b6f36
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-03-08 14:51:09 +0900

    SellItemCnectItem

--- app/controllers/sell_items_controller.rb
+++ app/controllers/sell_items_controller.rb
@@ -3,22 +3,28 @@ class SellItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
+    @sellitem = SellItem.new
   end
 
   def create
     @item = Item.new(item_params)
     respond_to do |format|
       if @item.save
+        @sellitem = SellItem.new(item_id:@item.id)
+        unless @sellitem.save
+          format.html { render :new, notice: 'ユーザー登録がされていません'}
+          format.json { render json: @item.errors, status: :unprocessable_entity }
+        end
         format.html { redirect_to "/transaction/buy/#{@item.id}", notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
         format.json { render json: @item.errors, status: :unprocessable_entity }
       end
+
     end
   end
 
-  # if文で分岐する、tureの場合商品停止、falseの場合再出品、
   def show
     item_foreignKey = Item.find(params[:id])
     @images = Image.where("item_id = #{item_foreignKey.id}")
@@ -29,8 +35,14 @@ class SellItemsController < ApplicationController
       if @item.update(item_params)
         item_foreignKey = Item.find(params[:id])
         @images = Image.where("item_id = #{item_foreignKey.id}")
-        format.html { redirect_to "/transaction/buy/#{@item.id}", notice: 'Item was successfully update.' }
+      if @item.exhibition === true 
+        format.html { redirect_to "/transaction/buy/#{@item.id}", notice: '出品を再開しました' }
         format.json { render :show, status: :created, location: @item }
+      else
+        @item.exhibition === false
+        format.html { redirect_to "/transaction/buy/#{@item.id}", notice: '商品を一旦停止しました' }
+        format.json { render :show, status: :created, location: @item }
+      end
       else
         format.html { render :edit }
         format.json { render json: @item.errors, status: :unprocessable_entity }
--- app/models/sell_item.rb
+++ app/models/sell_item.rb
@@ -1,6 +1,9 @@
 class SellItem < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :item
-
-  # validates :user_id, :item_id, :delibery_for, :arrival_to, :commition_fee,  presence: true,
+  # validates :user_id, optional: true
+  # validates :item_id, optional: true
+  # validates :delibery_for, optional: true
+  # validates :arrival_to, optional: true
+  # validates :commition_fee, optional: true
 end
