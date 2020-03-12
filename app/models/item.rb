c4ea16b9ba0220452625b920609cc4fc0bf2cc5b
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-12 21:59:54 +0900

    Modify カテゴリフォーム選択時にnilになるように

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,10 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :name, :money, presence: true
+  validates :name, :description, :money, :prefecture_address_id, \
+            :category_id, :item_condition_id, :shipping_method_id, \
+            :shipping_fee_id, :ship_date_id, :brand_id, presence: true
   validates :exhibition, :during_transaction, :soldout, inclusion: { in: [true, false] }
   validates :description, presence: true
-  validates :prefecture_address, presence: true
+  # validates :prefecture_address, presence: true
 end
--- db/migrate/20200215090811_create_items.rb
+++ db/migrate/20200215090811_create_items.rb
@@ -4,15 +4,15 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :description
       t.integer :money
-      t.boolean :exhibition, default: true, null: false
-      t.boolean :during_transaction, default: false, null: false
-      t.boolean :soldout, default: false, null: false
-      t.integer :prefecture_address_id, foreign_key: true, default: nil, null: false
-      t.integer :category_id, foreign_key: true, default: nil, null: false
-      t.integer :item_condition_id, foreign_key: true, default: nil, null: false
-      t.integer :shipping_fee_id, foreign_key: true, default: nil, null: false
-      t.integer :shipping_method_id, foreign_key: true, default: nil, null: false
-      t.integer :ship_date_id, foreign_key: true, default: nil, null: false
+      t.boolean :exhibition, default: true
+      t.boolean :during_transaction, default: false
+      t.boolean :soldout, default: false
+      t.integer :prefecture_address_id, foreign_key: true, default: nil
+      t.integer :category_id, foreign_key: true, default: nil
+      t.integer :item_condition_id, foreign_key: true, default: nil
+      t.integer :shipping_fee_id, foreign_key: true, default: nil
+      t.integer :shipping_method_id, foreign_key: true, default: nil
+      t.integer :ship_date_id, foreign_key: true, default: nil
       t.references :brand, foreign_key: true
       t.integer :size_id, foreign_key: true, default: nil
       t.timestamps
