31b7be4f2f0583f3d8f70a5585d17cb652189290
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-03-14 14:13:08 +0900

    FactoriesItems=>Sell_items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,18 +1,5 @@
 FactoryBot.define do
   factory :item do
-    name { 'MyString' }
-    description { 'MyString' }
-    money 1
-    soldout false
-    prefecture_address_id 1
-    category_id 1
-    item_condition_id 1
-    shipping_fee_id 1
-    shipping_method_id 1
-    ship_date_id 1
-    brand_id 1
-    size_id 1
-    exhibition 1
-    during_transaction 1
+ 
   end
 end
--- spec/factories/sell_items.rb
+++ spec/factories/sell_items.rb
@@ -6,5 +6,19 @@ FactoryBot.define do
     delibery_for false
     arrival_to false
     commition_fee 1
+    name { 'MyString' }
+    description { 'MyString' }
+    money 1
+    soldout false
+    prefecture_address_id 1
+    category_id 1
+    item_condition_id 1
+    shipping_fee_id 1
+    shipping_method_id 1
+    ship_date_id 1
+    brand_id 1
+    size_id 1
+    exhibition 1
+    during_transaction 1
   end
 end
