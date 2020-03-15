01afdfa1eb0854e92ad1e60a4c552a1115f78607
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-15 12:22:02 +0900

    Modify config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paths.add 'lib', eager_load: true
+  # config.paths.add 'lib', eager_load: true
 
 end
