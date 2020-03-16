d2741801577c0b4163305d751a82dfc0e6ca39ec
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-16 22:01:15 +0900

    Delete 未実装機能

--- app/assets/javascripts/component/category.js
+++
@@ -1,6 +0,0 @@
-// $(function(){
-//   result = document.getElementById('item_category_id').value
-//   $('#item_category_id').on('change', function(){
-//     document.getElementById('item_category_id').insertAdjacentHTML('afterend', '<select name="item[category_id]" id="item_category_id"></select>')
-//   })
-// })
