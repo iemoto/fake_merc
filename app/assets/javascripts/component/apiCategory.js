3507553dc106afc532326cca46a975029a9061f7
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-16 22:42:49 +0900

    Add 動的カテゴリフォーム

--- app/assets/javascripts/component/apiCategory.js
+++ app/assets/javascripts/component/apiCategory.js
@@ -1,40 +1,53 @@
 $(function(){
-  $('#item_category_id').on('change',apiData);
-  function apiData(){
+  $('#item_category_id').on('change', apiMainCategory);
+  function apiMainCategory(){
     $.ajax({
       method: 'GET',
       url: '/api/categories',
       dataType: 'json'
     })
-    .done(output)
+    .done(subCategory)
     .fail(function(){
       console.log('Error')
     })
 
-    function output(data){
-      console.log(data);
+    function subCategory(data){
       var selectData = document.getElementById("item_category_id").value
-      console.log(selectData)
-      var ancestryNumber = data.filter(function(item, index){
+      var ancestryNumber = data.filter(function(item){
         if (item.ancestry === `1/${selectData}`) return true;
       })
-      console.log(ancestryNumber);
-
       var html = '';
-      var hoge = ancestryNumber.forEach(
+      ancestryNumber.forEach(
         element => html += (`<option value=${element.id}>${element.name}</option>`)
         )
-        console.log(html)
-      $('#middleCategory').remove();
-      $('#item_category_id').after("<select name='item[category_id]' id='middleCategory' style='margin-top:25px;width:100%;height:48px;color: #222;background-color: white;border-radius: 4px;border: 1px solid #ccc;font-size: 16px;'>","</select>").ready(function(){
-        $('#middleCategory').append(html)
+          $('#middleCategory').css('display', 'block').append(html)
+    }
+  }
+
+  $(document).on('change', '#middleCategory', apiSubCategory);
+  function apiSubCategory(){
+    $.ajax({
+      method: 'GET',
+      url: '/api/categories',
+      dataType: 'json'
+    })
+    .done(subSubCategory)
+    .fail(function(){
+      console.log('Error')
+    })
+
+    function subSubCategory(data){
+      var selectData = document.getElementById("item_category_id").value
+      var selectSubData = document.getElementById("middleCategory").value
+      var ancestryNumber2 = data.filter(function(item){
+        if (item.ancestry === `1/${selectData}/${selectSubData}`) return true;
       })
-      // $('#item_category_id').innerHTML = `<h1>hello</h1>`
-      // $('#middleCategory').innerHTML = html;
-      // $('#item_category_id').after('</select>')
-      // console.log(data[0]);
-      // console.log(data[categories][0]);
-      // console.log(data[categories][0]);
+
+      var html = '';
+      ancestryNumber2.forEach(
+        element => html += (`<option value=${element.id}>${element.name}</option>`)
+        )
+      $('#subSubCategory').css('display','block').append(html)
     }
   }
 })
