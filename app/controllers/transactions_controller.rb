c722e0feb4af24a62ea7b8347847d24d506fbd22
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-20 19:38:28 +0900

    Add 商品購入時にテーブルに購入した商品情報を追加

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,6 +2,7 @@ class TransactionsController < ApplicationController
   def buy
     @item = Item.find(params[:id])
     @card = Card.where(user_id: current_user.id).first
+    @transact = SellMoney.create(user_id: current_user.id, sell_item_id: @item.id, sell_money: @item.money)
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     customer = Payjp::Customer.retrieve(@card.customer_id)
 
@@ -13,5 +14,6 @@ class TransactionsController < ApplicationController
       currency: 'jpy'
     )
     redirect_to root_path
+    flash[:notice] = '商品の購入を完了しました'
   end
 end
