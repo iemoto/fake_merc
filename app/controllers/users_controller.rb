80d72e67eee84b7d69a69cf24f76d3a114cd096d
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-03-20 17:30:50 +0900

    Add 住所の更新機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,5 +17,23 @@ class UsersController < ApplicationController
   def edit
     add_breadcrumb 'マイページ', mypage_path
     add_breadcrumb '本人情報の登録'
+    @personal = PersonalUser.find_by(user_id: current_user.id)
+  end
+
+  def update
+    @personal = PersonalUser.find_by(user_id: current_user.id)
+    @personal.update(address_params)
+    if @personal.save
+     redirect_back(fallback_location: root_url)
+     flash[:notice] = '住所情報を更新しました'
+    else
+     redirect_back(fallback_location: root_url)
+     flash[:notice] = '住所情報の更新に失敗しました'
+    end
+  end
+
+  private
+  def address_params
+    params.require(:personal_user).permit(:post_number, :street_address, :prefecture_address_id, :municipality_address, :building_name_adress, :building_name_address)
   end
 end
