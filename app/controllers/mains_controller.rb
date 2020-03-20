78096d75d074f8433a9ae41b949659b532a0f642
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-03-20 22:36:04 +0900

    item値出力

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,7 +1,15 @@
 class MainsController < ApplicationController
+  before_action :set_item ,only:[:show]
+
   def index
   end
 
   def show
   end
+end
+
+private
+
+def set_item
+  @item = Item.find(params[:id])
 end
